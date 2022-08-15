186ae046894d8d7fdb3ae7ea5c7af2218884f8a0
Sarthak Mohanty <sarthak@sarthakmohanty.me>
2022-08-15 15:32:41 -0500

    adds shapes and font

--- README.md
+++ README.md
@@ -1,11 +1,4 @@
-# Perfect
-The best Next.js boilerplate ever.
-# What does this have?
-- Tailwind CSS (no more going through the installation guide!) - for styling components easily
-- `next-themes` (@pacocoursey) - for going to the dark side
-- `swr` (@vercel) - for fetching constantly mutating data
-  - `fetcher.js` so you don't have to keep writing return functions
-- `framer-motion` - for making interactive components and eye-catching animations
-- `react-icons` - for including your favorite icon packs without increasing bundle size significantly. Tree shaking included.
+# shape-playground
+playing around with shapes
 
-&copy; 2021 Sarthak Mohanty. Licensed under the [Unlicense](LICENSE) from Mozilla.
+&copy; 2021 Sarthak Mohanty. Unlicensed. Do whatever you want with this.
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,35 @@
-import HeadObject from '../components/head'
-import Nav from '../components/nav';
+import shuffle from "../lib/shuffle"
+import { motion } from "framer-motion"
+import { useRef } from "react";
+import random from "../lib/random"
 
 export default function Home() {
+  let shapes = [...Array(29)];
+  shapes = shapes.map((item, i) => item = {
+    src: `shape-${i + 1}.svg`,
+    rotate: random(0, 180),
+    scale: random(0.3, 2),
+    x: `${random(0, 100)}%`,
+    y: `${random(0, 100)}%`
+  })
+  let shapes_plural = shapes.concat([...shapes], [...shapes], [...shapes], [...shapes], [...shapes], [...shapes]);
+  shuffle(shapes_plural);
+  const containerRef = useRef(null)
   return (
-    <div className="dark:text-white dark:bg-black">
-      <HeadObject>
-        {/* You can put extra tags in here, or leave it blank */}
-      </HeadObject>
-      <Nav />
-      The perfect Next.js Theme. Contains everything. Check README for more information.
-    </div>
+    <>
+      <motion.div className="absolute z-0 top-0 left-0 h-screen w-screen flex flex-wrap gap-x-5" ref={containerRef}>
+      <h1 className="w-fitcontent text-7xl p-5 pr-10 pb-0">Sarthak Mohanty</h1>
+          {shapes_plural.map((shape, index) => (
+            <motion.div drag dragConstraints={containerRef} key={index}>
+              <img className="pointer-events-none"
+              style={{
+                transform: `rotate("${shape.rotate}deg")`,
+                margin: "0 0 -10em 0"
+              }}
+              src={`/shapes/${shape.src}`} />
+            </motion.div>
+          ))}
+      </motion.div>
+    </>
   )
 }
