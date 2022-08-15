7d8f392dfcec80a13be5fb5f3929e8142536cba6
Sarthak Mohanty <sarthak@sarthakmohanty.me>
2022-08-15 15:52:57 -0500

    fixed duplication and shuffle alg

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import shuffle from "../lib/shuffle"
-import { motion } from "framer-motion"
+import { motion, transform } from "framer-motion"
 import { useRef } from "react";
 import random from "../lib/random"
 
@@ -7,23 +7,26 @@ export default function Home() {
   let shapes = [...Array(29)];
   shapes = shapes.map((item, i) => item = {
     src: `shape-${i + 1}.svg`,
+  })
+  shapes = shuffle(shapes.concat([...shapes], [...shapes], [...shapes], [...shapes], [...shapes], [...shapes])).splice(0, 50).map((item, i) => item = {...item,
     rotate: random(0, 180),
     scale: random(0.3, 2),
-    x: `${random(0, 100)}%`,
-    y: `${random(0, 100)}%`
+    x: random(-1000, 1000),
+    y: random(-1000, 1000)
   })
-  let shapes_plural = shapes.concat([...shapes], [...shapes], [...shapes], [...shapes], [...shapes], [...shapes]);
-  shuffle(shapes_plural);
+  console.log(shapes)
   const containerRef = useRef(null)
   return (
     <>
       <motion.div className="absolute z-0 top-0 left-0 h-screen w-screen flex flex-wrap gap-x-5" ref={containerRef}>
       <h1 className="w-fitcontent text-7xl p-5 pr-10 pb-0">Sarthak Mohanty</h1>
-          {shapes_plural.map((shape, index) => (
+          {shapes.map((shape, index) => (
             <motion.div drag dragConstraints={containerRef} key={index}>
               <img className="pointer-events-none"
               style={{
-                transform: `rotate("${shape.rotate}deg")`,
+                transform: `
+                rotate("${shape.rotate}deg")
+                translate(${shape.x}, ${shape.y})`,
                 margin: "0 0 -10em 0"
               }}
               src={`/shapes/${shape.src}`} />
